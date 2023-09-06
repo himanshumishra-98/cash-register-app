7da770b36861d4c57ba64c928a56277a42c474f8
Himanshu Mishra <himanshu.pmishra555@gmail.com>
2023-09-06 23:51:47 +0530

    created app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+let billAmount = document.querySelector("#bill-amount");
+let cashGiven = document.querySelector("#cash-given");
+let errorMessage = document.querySelector("error-message");
+let buttonCheck = document.querySelector("#button-check");
+
+let notes = [500,200,100,50,20,10,5,2,1]
+
+let amountToBeReturn = cashGiven.value - billAmount.value;
+
+function showNotes() {
+    if(amountToBeReturn)
+}
+
+function errMesgBill() {
+    return "Invalid Bill Amount"
+}
+
+function errMesgCash() {
+    return "Invalid Cash Amount"
+}
+
+buttonCheck.addEventListener("click",showNotes);
